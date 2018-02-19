bf463fb235a02ea54bba98d334bd2e478cba9bfd
Stephen Bohner <sbohner14335@users.noreply.github.com>
2018-02-18 20:49:42 -0500

    Maven configured

---
+++ src/main/java/grephy/grephy.java
@@ -0,0 +1,9 @@
+package grephy;
+
+public class grephy {
+	
+	public static void main(String[] args) {
+		System.out.println("hello grephy");
+	}
+	
+}
---
+++ src/test/java/grephy/grephyTest.java
@@ -0,0 +1,10 @@
+package grephy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class grephyTest {
+
+
+}
